135b4a164e8ce20cdd543ef56a72c6d83ba3637f
Takewood-ai <antonchegur313@gmail.com>
2026-08-02 17:43:56 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,17 @@
+text = open("data.txt", "r", encoding="utf-8").read()
 
+words = text.split()
+
+model = {}
+
+for i in range(len(words)-1):
+    word = words[i]
+    next_word = words[i+1]
+
+    if word not in model:
+        model[word] = []
+
+    model[word].append(next_word)
+
+print("T-K-W обучен!")
+print(model)
