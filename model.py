ebe1942070a49fb796dbae230951a7c9e2aebdf2
Takewood-ai <antonchegur313@gmail.com>
2026-08-02 19:02:24 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,30 @@
+import json
 
+def train(text):
+    words = text.split()
+
+    model = {}
+
+    for i in range(len(words)-1):
+        word = words[i]
+        next_word = words[i+1]
+
+        if word not in model:
+            model[word] = []
+
+        model[word].append(next_word)
+
+    return model
+
+
+def save_model(model):
+    with open("memory.json", "w", encoding="utf-8") as file:
+        json.dump(model, file, ensure_ascii=False)
+
+
+def load_model():
+    try:
+        with open("memory.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except:
+        return {}
